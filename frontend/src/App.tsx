0ca0142de3fa8e6b03849f6689019165ee59ec0c
Ali-Olliek <Olliek95@gmail.com>
2024-09-05 15:36:08 +0300

    Added Routes to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,45 @@
-import React from 'react';
 import './App.css';
+import Redirector from './navigation/Redirector';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  SignInScreen,
+  SignUpScreen,
+  StudentsScreen,
+  UsersScreen,
+} from './screens';
+import MainLayout from './components/layouts/main-layout/MainLayout';
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Routes>
+        {/* Public Routes */}
+        <Route
+          path='/sign-in'
+          element={
+            <MainLayout>
+              <SignInScreen />
+            </MainLayout>
+          }
+        />
+        <Route
+          path='/sign-up'
+          element={
+            <MainLayout>
+              <SignUpScreen />
+            </MainLayout>
+          }
+        />
+
+        <Route path='/' element={<Redirector />}>
+          {/* Protected Routes */}
+          <Route path='/students' element={<StudentsScreen />} />
+          <Route path='/users' element={<UsersScreen />} />
+        </Route>
+        <Route path='*' element={<>404</>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
