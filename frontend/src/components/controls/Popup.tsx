3608c289bae397ca5a3e232fcfb584e32e72b1f1
Ali-Olliek <Olliek95@gmail.com>
2024-09-06 12:08:28 +0300

    Styled Popup

--- frontend/src/components/controls/Popup.tsx
+++ frontend/src/components/controls/Popup.tsx
@@ -1,21 +1,29 @@
+import './popup.css';
 import React from 'react';
 
 interface IPopupProps {
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
   children: React.ReactNode;
+  title: string;
 }
 
-function Popup({ open, children, setOpen }: IPopupProps) {
+function Popup({ open, children, setOpen, title }: IPopupProps) {
   return (
-    <dialog
-      style={{ height: '45vh', width: '30vw' }}
-      title='Update Student Information'
-      open={open}
-    >
-      <button onClick={() => setOpen(false)}>x</button>
-      {children}
-    </dialog>
+    <div className='dialog-wrapper'>
+      {open && (
+        <div className='dialog-overlay' onClick={() => setOpen(false)}>
+          <dialog className='pop-up' open>
+            <div>
+              <h4>{title}</h4>
+              <button onClick={() => setOpen(false)}>x</button>
+            </div>
+            <hr />
+            {children}
+          </dialog>
+        </div>
+      )}
+    </div>
   );
 }
 
